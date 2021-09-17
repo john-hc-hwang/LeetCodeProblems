9d48876cfe53a846e3f19bf2ce45e68ddc58e786
John Hwang <johnhchwang@gmail.com>
2021-09-17 15:52:43 -0700

    comments added for review

--- strStr.js
+++ strStr.js
@@ -1,4 +1,6 @@
-var strStr = function (haystack, needle) {
+// strStr is essentially the same as indexOf method of string
+
+const strStr = (haystack, needle) => {
   if (needle === '') return 0;
 
   const i = haystack.indexOf(needle);
